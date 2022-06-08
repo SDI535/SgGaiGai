c7b7872b0127703819c9c0c45b3db3b22ae7ac0c
enirevlow <foowinlin@gmail.com>
2022-06-08 15:29:34 +0800

    update taxi changes

--- src/Screens/Header/Header.js
+++ src/Screens/Header/Header.js
@@ -1,81 +1,56 @@
 import React from "react";
-import './Main.css';
-import ColorLogo from './Logo/LogoC.svg';
-import Footer from '../Footer/Footer';
-import Taxi from '../../Components/Taxi/Taxi';
+import "./Main.css";
+import ColorLogo from "./Logo/LogoC.svg";
+import Footer from "../Footer/Footer";
+import Taxi from "../../Components/Taxi/Taxi";
 import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    NavLink,
-    Redirect
-  } from "react-router-dom";
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  NavLink,
+  Redirect,
+} from "react-router-dom";
+import Body from "../Body/Body";
 
 function Header() {
-    return (
-        <>
-            <div className="Header">
+  return (
+    <>
+      <div className="Header">
+        <div className="Route">
+          <div className="logo">
+            <img src={ColorLogo} alt="SG Gai Gai" width={150} />
+          </div>
 
-                <div className="Route">
-                    <Router>
-
-                        <div className="logo">
-                            <img src={ColorLogo} alt="SG Gai Gai" width={150}/>
-                        </div>
-
-                        <div class="tab-selection">
-                            
-                            <div>
-                            <NavLink activeClassName="active" to="/bus">Bus</NavLink>
-                            </div>
-
-                            <div>
-                            <NavLink activeClassName="active" to="/car">Car</NavLink>
-                            </div>
-
-                            <div>
-                            <NavLink activeClassName="active" to="/taxi">Taxi</NavLink>
-                            </div>
-
-                            <div>
-                            <NavLink activeClassName="active" to="/train">Train</NavLink>
-                            </div>
-
-                        </div>
-
-                        <div className="Divider"></div>
-
-                        <div class="tab-container">
-                            <Switch>
-
-                            <Redirect exact from="/" to="bus" />
-
-                            <Route path="/bus">
-                                <div>Bus Content</div>
-                            </Route>
-
-                            <Route path="/car">
-                                <div>Car Content</div>
-                            </Route>
-
-                            <Route path="/taxi">
-                                <Taxi />
-                            </Route>
-
-                            <Route path="/train">
-                                <div>Train Content</div>
-                            </Route>
-
-                            </Switch>
-                        </div>
-                    </Router>
-                </div>
+          <div class="tab-selection">
+            <div>
+              <NavLink activeClassName="active" to="/Bus">
+                Bus
+              </NavLink>
             </div>
 
-            <Footer />
-        </>
-    )
-}
+            <div>
+              <NavLink activeClassName="active" to="/Car">
+                Car
+              </NavLink>
+            </div>
 
+            <div>
+              <NavLink activeClassName="active" to="/Taxi">
+                Taxi
+              </NavLink>
+            </div>
+
+            <div>
+              <NavLink activeClassName="active" to="/Train">
+                Train
+              </NavLink>
+            </div>
+          </div>
+          <div className="Divider"></div>
+        </div>
+      </div>
+    </>
+  );
+}
 
 export default Header;
