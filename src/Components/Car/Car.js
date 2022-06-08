40183577f9b13ccef97554218d2eca9ea62b24c3
enirevlow <foowinlin@gmail.com>
2022-06-08 19:53:48 +0800

    update car for routing

--- src/Components/Car/Car.js
+++ src/Components/Car/Car.js
@@ -1,34 +1,33 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import LTAAPI from "../API/LTAAPI";
 import CarList from "./CarList";
 import CarMap from "./CarMap";
 //import SearchCarpark from "./SearchCarpark";
 
-const Car = () => {
-    const [carparkList, setCarParkList] = useState([]);
+const Car = ({ isListView }) => {
+  const [carparkList, setCarParkList] = useState([]);
 
-    
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await LTAAPI.get("/CarParkAvailabilityv2");
-                setCarParkList(response.data.value);
-            } catch (err) {
-                console.log(err);
-            }
-        };
-        fetchData();
-    }, []);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await LTAAPI.get("/CarParkAvailabilityv2");
+        setCarParkList(response.data.value);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchData();
+  }, []);
 
-    return (
-        <>
-        <div>
-        <h1>Carpark Availability</h1>
-        <CarList  carparkList={carparkList}/>
-        <CarMap data={carparkList}/>
-        </div>
-        </>
-    )
-}
+  return (
+    <>
+      {isListView ? (
+        <CarList carparkList={carparkList} />
+      ) : (
+        <CarMap data={carparkList} />
+      )}
+    </>
+  );
+};
 
 export default Car;
--- src/Components/Car/CarList.js
+++ src/Components/Car/CarList.js
@@ -1,32 +1,28 @@
 import React from "react";
 
-const CarList = ({
-    carparkList,
-    filteredCarparkList,
-}) => {
-    const fullCarparkList = carparkList.map((carparkList,key) => {
-        return (
-            <tr key={key}>
-                <td>{carparkList.Development}</td>
-                <td>{carparkList.AvailableLots}</td>
-            </tr>
-        )
-    })
-
+const CarList = ({ carparkList, filteredCarparkList }) => {
+  const fullCarparkList = carparkList.map((carparkList, key) => {
     return (
-        <>
-        <h3>Full Carpark List</h3>
-        <table>
-            <tbody>
-            <tr>
-                <th>Location</th>
-                <th>Available Lots</th>
-            </tr>
-                {fullCarparkList}
-            </tbody>
-        </table>
-        </>
-    )
-}
+      <tr key={key}>
+        <td>{carparkList.Development}</td>
+        <td>{carparkList.AvailableLots}</td>
+      </tr>
+    );
+  });
+
+  return (
+    <>
+      <table>
+        <tbody>
+          <tr>
+            <th>Location</th>
+            <th>Available Lots</th>
+          </tr>
+          {fullCarparkList}
+        </tbody>
+      </table>
+    </>
+  );
+};
 
 export default CarList;
